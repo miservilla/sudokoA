cfc647bbfcb9b6b728f5e166b16fb6481d0beb68
miservilla <miservilla@gmail.com>
2017-09-20 20:15:55 -0600

    Back on track. Still need to complete unit block testing for both input and solving.

--- main.c
+++ main.c
@@ -15,6 +15,7 @@ void buildpuzz(char puzzLine[]);
 void inputErrorTest();
 void dupTestRow(char puzz[9][9]);
 void dupTestCol(char puzz[9][9]);
+void dupTestUnit(char puzz[9][9], int row, int col, int num);
 
 char puzzLine[81];
 char puzz[9][9];
@@ -23,7 +24,10 @@ int puzzChar;
 int charCount = 0;
 int row = 0;
 int col = 0;
+int num;
 int errorFlag = 0;
+int i;
+int j;
 
 
 int main()
@@ -55,7 +59,6 @@ int main()
 }
 void cleanPuzz(char puzzLine[], char puzz[9][9])
 {
-  int i;
   for (i = 0; i < 81; ++i)
   {
     puzzLine[i] = ' ';
@@ -102,20 +105,17 @@ void buildpuzz(char puzzLine[])
 }
 void dupTestRow(char puzz[9][9])
 {
-  int i;
-  int cmpInt;
-  int loc;
-  for (loc = 0; loc < 9; ++loc) /*1st loop to advance down rows.*/
+  for (row = 0; row < 9; ++row) /*1st loop to advance down rows.*/
   {
-    for (i = 0; i < 8; ++i) /*2nd loop to test across row.*/
+    for (col = 0; col < 8; ++col) /*2nd loop to test across row.*/
     {
-      if (puzz[loc][i] == '.')
+      while(puzz[row][col] == '.' && col < 8)
       {
-        break;
+        ++col;
       }
-      for (cmpInt = i + 1; cmpInt < 9; ++cmpInt) /*3rd loop to compare chars.*/
+      for (num = col + 1; num < 9; ++num) /*3rd loop to compare chars.*/
       {
-        if (puzz[loc][i] == puzz[loc][cmpInt])
+        if (puzz[row][col] == puzz[row][num])
         {
           errorFlag = 1;
           return;
@@ -126,20 +126,20 @@ void dupTestRow(char puzz[9][9])
 }
 void dupTestCol(char puzz[9][9])
 {
-  int i;
-  int cmpInt;
-  int loc;
-  for (loc = 0; loc < 9; ++loc) /*1st loop to advance down cols.*/
+  for (col = 0; col < 9; ++col) /*1st loop to advance down cols.*/
   {
-    for (i = 0; i < 8; ++i) /*2nd loop to test across col.*/
+    for (row = 0; row < 8; ++row) /*2nd loop to test across col.*/
     {
-      if (puzz[i][loc] == '.')
+      if (puzz[row][col] == '.')
       {
-        break;
+        while(puzz[row][col] == '.' && row < 8)
+        {
+          ++row;
+        }
       }
-      for (cmpInt = i + 1; cmpInt < 9; ++cmpInt) /*3rd loop to compare chars.*/
+      for (num = row + 1; num < 9; ++num) /*3rd loop to compare chars.*/
       {
-        if (puzz[i][loc] == puzz[cmpInt][loc])
+        if (puzz[row][col] == puzz[num][col])
         {
           errorFlag = 1;
           return;
@@ -147,4 +147,19 @@ void dupTestCol(char puzz[9][9])
       }
     }
   }
+}
+void dupTestUnit(char puzz[9][9], int row, int col, int num)
+{
+  int rowStart = row/3 * 3;
+  int colStart = col/3 * 3;
+  for (i = rowStart; i < rowStart + 3; ++i)
+  {
+    for (j = colStart; j < colStart + 3; ++j)
+    {
+      if(puzz[i][j] == num)
+        errorFlag = 1;
+      return;
+    }
+  }
+
 }
