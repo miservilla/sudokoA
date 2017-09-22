46c6ae3c704707f9e27e0ad1d933e1c2218bdcce
miservilla <miservilla@gmail.com>
2017-09-21 19:11:03 -0600

    Attempting to solve.

--- main.c
+++ main.c
@@ -17,9 +17,12 @@ void dupTestRow(char puzz[9][9]);
 void dupTestCol(char puzz[9][9]);
 void dupTestUnit(char puzz[9][9], int row, int col, int num);
 void threeSquareErrorSearch(char puzz[9][9]);
+void solver(char puzz[9][9]);
+void buildUsedNumArray(char puzz[9][9], int row, int col);
 
 char puzzLine[81];
 char puzz[9][9];
+int usedNumArray[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 int charCount;
 int puzzChar;
 int charCount = 0;
@@ -52,6 +55,7 @@ int main()
       dupTestCol(puzz);
       threeSquareErrorSearch(puzz);
       inputErrorTest();
+      solver(puzz);
       displayPuzz(puzz); /*This needs to come out in prod.*/
       cleanPuzz(puzzLine, puzz);
       charCount = 0;
@@ -173,7 +177,62 @@ void threeSquareErrorSearch(char puzz[9][9])
     for (col = 0; col < 9; ++col)
     {
       if(puzz[row][col] != '.')
-      dupTestUnit(puzz, row, col, puzz[row][col]);
+        dupTestUnit(puzz, row, col, puzz[row][col]);
+    }
+  }
+}
+void solver(char puzz[9][9])
+{
+  for (row = 0; row < 9; ++row)
+  {
+    for (col = 0; col < 9; ++col)
+    {
+      if (puzz[row][col] == '.')
+      {
+        buildUsedNumArray(puzz, row, col);
+        for (i = 1; i < 10; ++i)
+        {
+          if (usedNumArray[i] == 0)
+          {
+            puzz[row][col] = i + 48;
+            break;
+          }
+          else printf("I'm stuck!");
+        }
+      }
+    }
+  }
+}
+void buildUsedNumArray(char puzz[9][9], int row, int col)
+{
+  int rowStart;
+  int colStart;
+
+  for (i = 0; i < 10; ++i)
+  {
+    usedNumArray[i] = 0;
+  }
+  for (i = 0; i < 9; ++i)
+  {
+    if (isdigit(puzz[row][i]))
+    {
+      usedNumArray[puzz[row][i] - 48] = 1;
+    }
+    if (isdigit(puzz[i][col]))
+    {
+      usedNumArray[puzz[i][col] - 48] = 1;
+    }
+  }
+  rowStart = row/3 * 3;
+  colStart = col/3 * 3;
+  for (i = rowStart; i < rowStart + 3; ++i)
+  {
+    for (j = colStart; j < colStart + 3; ++j)
+    {
+      if(isdigit(puzz[i][j]))
+      {
+        usedNumArray[puzz[i][j] - 48] = 1;
+      }
     }
   }
 }
